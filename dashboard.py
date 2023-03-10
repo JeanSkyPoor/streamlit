7f5bd8da3ebc2572daed13b32f6aa484ffc74584
Jean_Sky_Poor <katovano2705@gmail.com>
2023-03-10 21:07:31 +0300

    Some updates

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
-import altair as alt
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+
 import plotly.express as px
 
 def get_metrics(df):
